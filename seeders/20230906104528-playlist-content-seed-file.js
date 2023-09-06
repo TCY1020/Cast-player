bd1dc3d5c757de1ee24b4cb78585a943f35a5418
TCY1020 <a25023423@gmail.com>
2023-09-06 19:23:17 +0800

    feat: create playlistContent seed

---
+++ seeders/20230906104528-playlist-content-seed-file.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const Playlists = await queryInterface.sequelize.query(
+      'SELECT * FROM Playlists;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const Podcasters = await queryInterface.sequelize.query(
+      'SELECT id FROM Podcasters',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    ) // Podcasters = [{id:1}, {id:2}....]
+    const list = []
+    Playlists.forEach(Playlist => {
+      for (let i = 0; i < 6; i++) {
+        list.push(Playlist.id)
+      }
+    })
+    await queryInterface.bulkInsert('PlaylistContents',
+      Array.from({ length: list.length }, (_, index) => ({
+        Playlist_id: list[index],
+        Podcaster_id: Podcasters[Math.floor(Math.random() * Podcasters.length - 1)].id,
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('PlaylistContents', {})
+  }
+}
